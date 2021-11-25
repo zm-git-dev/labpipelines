82f73cc6b5ccbd74efdd01b7095774c9311d6d69
Wanding Zhou - Bioinformatics <zhouwanding@gmail.com>
2021-11-25 09:00:23 -0500

    Update chip_seq.sh

--- src/ChIP_Seq/chip_seq.sh
+++ src/ChIP_Seq/chip_seq.sh
@@ -212,6 +212,38 @@ macs2 callpeak -t bam/$targetname.bam -c bam/$controlname.bam -f BAM -g $WZSEQ_M
     done
 }
 
+function macs2mergeNoControl() {
+  cmd='
+base=$("pwd")
+cd ${base}
+mkdir -p macs2Merged
+macs2 callpeak -t filtered_bam/'$sname'*.filtered.bam -g mm -q 0.05 -n '$sname' --outdir macs2Merged -B
+#ls filtered_bam/${arg1}*.filtered.bam
+'
+  jobname="macs2mergeNoControl_"$sname
+}
+
+function runMacs2() {
+  cmd='
+base=$("pwd")
+cd ${base}
+mkdir -p macs2
+macs2 callpeak -t filtered_bam/'$sname'*.filtered.bam -g mm -q 0.05 -n '$sname' --outdir macs2 -B --broad
+#ls filtered_bam/${arg1}*.filtered.bam
+'
+  jobname="runMacs2_"$sname
+}
+
+function runMacs2WithControl() {
+  cmd='
+base=$("pwd")
+cd ${base}
+mkdir -p macs2
+macs2 callpeak -t filtered_bam/'$sname'.filtered.bam -c filtered_bam/'$control' -g mm -q 0.05 -n '$sname' --outdir macs2 -B --broad
+'
+  jobname="macs2WithControl_"$sname
+}
+
 function chipseq_gem {
 
   base=$(pwd);
