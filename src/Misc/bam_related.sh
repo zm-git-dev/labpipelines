38aa2deda5b77b5cf75b59357f24916c2fbdefdb
Wanding Zhou - Bioinformatics <zhouwanding@gmail.com>
2021-11-25 09:01:34 -0500

    Update bam_related.sh

--- src/Misc/bam_related.sh
+++ src/Misc/bam_related.sh
@@ -2,6 +2,28 @@
 ## others
 #########
 
+function bam2bigwig()
+{
+  cmd='
+base=$("pwd")
+cd ${base}
+mkdir -p bigwig
+if [[ '$species' == "hs" ]]; then
+	gsize=2790000000
+elif [[ '$species' == "mm" ]]; then
+	gsize=1870000000
+else
+	gsize=2000000000
+fi
+if [ ! -f filtered_bam/'$sname'.filtered.bam.bai ]; then
+  samtools index filtered_bam/'$sname'.filtered.bam
+fi
+bamCoverage -b filtered_bam/'$sname'.filtered.bam -o bigwig/'$sname'.bw -of bigwig -p '$ppn' -bs 20 --effectiveGenomeSize ${gsize} --normalizeUsing RPKM --minMappingQuality 20 -bl /home/dingw1/Mytools/data/mm10-blacklist.v2.bed
+#--ignoreDuplicates
+'
+	jobname="bam2bigwig_"$sname
+}
+
 ## assume chrm, beg, end, beta, coverage
 function wzseq_cov5 {
   f=$1
